2412d8c8a1299c211950e2b983ecaefd4261cc87
ragavan28 <ragavvenkat269@gmail.com>
2026-06-24 09:32:46 +0530

    fix: trim certification list

--- src/data/certifications.js
+++ src/data/certifications.js
@@ -1,30 +1,26 @@
 export const certifications = [
   {
-    title: "TensorFlow Developer Certificate",
-    issuer: "Google",
-    date: "Issued Dec 2024",
-    credentialId: "TF-8942-88",
-    link: "https://www.credential-link.com"
+    title: 'Industrial Training & Internship Certificate',
+    issuer: 'Sri Eshwar College of Engineering, Coimbatore',
+    date: 'Issued Jan 2025',
+    link: 'https://drive.google.com/file/d/1uf4gKEG7t3d77AQ7bfuLxLZDPjbE2Z6Z/view?usp=drive_link',
   },
   {
-    title: "AWS Certified Solutions Architect – Associate",
-    issuer: "Amazon Web Services (AWS)",
-    date: "Issued Aug 2024",
-    credentialId: "AWS-ASA-9941",
-    link: "https://www.credential-link.com"
+    title: 'ICERCS\'24 Paper Presentation Certificate',
+    issuer: 'Karpagam Academy of Higher Education, Coimbatore',
+    date: 'Issued Dec 2024',
+    link: 'https://drive.google.com/file/d/16tAoHmvRGwCBleOGVFcbC8p5QmKSHqjx/view?usp=drive_link',
   },
   {
-    title: "Deep Learning Specialization",
-    issuer: "DeepLearning.AI",
-    date: "Issued May 2024",
-    credentialId: "DL-SPEC-7731",
-    link: "https://www.credential-link.com"
+    title: 'Cloud Computing',
+    issuer: 'NPTEL',
+    date: 'Issued 2024',
+    link: 'https://drive.google.com/file/d/1gzK5caDdRCehGXXD1x4Kpu1uFIv4K_aS/view?usp=drive_link',
   },
   {
-    title: "MongoDB Certified Developer Associate",
-    issuer: "MongoDB",
-    date: "Issued Feb 2024",
-    credentialId: "MDB-CDA-4810",
-    link: "https://www.credential-link.com"
-  }
+    title: 'Business Fundamentals for Entrepreneurs',
+    issuer: 'NPTEL',
+    date: 'Issued 2024',
+    link: 'https://drive.google.com/file/d/1gzK5caDdRCehGXXD1x4Kpu1uFIv4K_aS/view?usp=drive_link',
+  },
 ];
